1d07c2451099ded9cc1c00abac86759096c19d61
Lartaxx <didou.com@gmail.com>
2023-05-10 13:55:45 +0200

    Many changes

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 import './bootstrap';
+
+const burgerButton = document.querySelector('[data-collapse-toggle="navbar-default"]');
+  const menu = document.getElementById('navbar-default');
+
+  // Ajoutez un écouteur d'événements pour le clic sur le bouton du menu burger
+  burgerButton.addEventListener('click', () => {
+    // Basculez la classe 'hidden' pour afficher ou masquer le menu
+    menu.classList.toggle('hidden');
+  });
--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -17,7 +17,7 @@
         <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
       </button>
       <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-        <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+        <ul class="font-semibold text-lg flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
           <li>
             <a href="#" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
           </li>
@@ -25,13 +25,10 @@
             <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
           </li>
           <li>
-            <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
-          </li>
-          <li>
-            <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
-          </li>
-          <li>
             <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+          </li>
+          <li>
+            <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Github</a>
           </li>
         </ul>
       </div>
@@ -39,19 +36,23 @@
   </nav>
 
   <div class="container">
+
     <div class="text-center text-white">
-        <h1 class="text-2xl font-bold">Hi, I'm Lartaxx 👋!</h1>
-        <p class="text-4xl mt-5 font-semibold">
+        <h1 class="text-4xl font-bold">Hi, I'm Lartaxx 👋!</h1>
+        <p class="text-6xl mt-5 font-semibold">
             Back-end web
             <span class="linear-gradient">developer</span>
         </p>
-        <p class="text-4xl font-semibold mt-2">
+        <p class="text-6xl font-semibold mt-2">
             From
             <span class="french-gradient">France</span>
         </p>
     </div>
 
-  </div>
-  
+    <hr class="h-px my-8 linear-gradient border-0 w-3/4 mx-auto">
+
+
+</div>
+@vite('resources/js/app.js')
 </body>
 </html>
